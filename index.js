64e8db5e382fdb2c30f3cb226aa38f4f5adedb07
jayandra135 <jayandra135@gmail.com>
2023-10-22 15:59:30 +0530

    mongo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import RegisterRouter from "./router/register.router";
 var app = express();
 const PORT = process.env.PORT || 8001;
@@ -11,14 +11,27 @@ var corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+const uri = process.env.DB_NAME;
 app.use(cors(corsOptions));
 app.use(express.static(__dirname));
 app.listen(PORT, () => {
   console.log("Your server running on http://localhost:" + PORT);
 });
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/" + process.env.DB_NAME)
-  .then(() => console.log("Connected!"));
-
 app.use("/register", RegisterRouter);
+
+async function main() {
+  const uri = process.env.DATABASE;
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("connected to mongodb");
+    })
+    .catch(() => {
+      console.log("could not connect to mongodb");
+    });
+}
+main();
